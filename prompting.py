067b5f304476203b168e87de02d87e0b80d368d0
Egor <94971220+Eguarundi@users.noreply.github.com>
2025-02-24 22:01:40 +0700

    create instruction template.py

--- prompting.py
+++ prompting.py
@@ -4,10 +4,15 @@ import numpy as np
 
 
 class PromptBuilder:
-    def __init__(self, prompt_prefix=''):
-        self.__prompt = prompt_prefix
+    def __init__(self, question):
+        self.__prompt = ''
+        self.__question = question
+        self.__few_shot = None
+        self.__schema_template = None
+        self.__cell_value_referencing = None
 
-    def add_few_shot(self, sentence_model, target_question, queries, message='Example of similar tasks and answers for them'):
+
+    def add_few_shot(self, sentence_model, target_question, queries):
         questions = [sample['question_ru'] for sample in queries]
 
         input_examples = []
@@ -22,11 +27,10 @@ class PromptBuilder:
             few_shot_template += f'Q: {ex[0]}\n'
             few_shot_template += f'A: {ex[1]}\n'
 
-        self.__prompt += f'\n### {message}\n'
-        self.__prompt += few_shot_template
+        self.__few_shot = few_shot_template
         return self
 
-    def add_schema_template(self, db_conn, message='Tables schema:'):
+    def add_schema_template(self, db_conn):
         sql_master = pd.read_sql('SELECT * FROM sqlite_master', db_conn)
         tables = sql_master[sql_master['type'] == 'table']['name']
         hier = []
@@ -40,11 +44,10 @@ class PromptBuilder:
         schema_template = ''
         for table in hier:
             schema_template += f"{table['table_name']}({', '.join(table['struct'])});\n"
-        self.__prompt += f'\n### {message}\n'
-        self.__prompt += schema_template
+        self.__schema_template = schema_template
         return self
 
-    def add_cell_value_referencing(self, db_conn, count=1, message='Example of rows in table:'):
+    def add_cell_value_referencing(self, db_conn, count=1):
         sql_master = pd.read_sql('SELECT * FROM sqlite_master', db_conn)
         tables = sql_master[sql_master['type'] == 'table']['name']
 
@@ -63,13 +66,72 @@ class PromptBuilder:
         for data in data_information:
             value_template += f"{data['table_name']}({', '.join(data['examples'])});\n"
 
-        self.__prompt += f'\n### {message}\n'
-        self.__prompt += value_template
-        return self
-        
-    def add_message(self, message):
-        self.__prompt += f'\n### {message}\n'
+        self.__cell_value_referencing = value_template
         return self
 
-    def build_prompt(self):
+    def include_target(self, number: int):
+        Variations = {
+            1: 'Ответь на вопрос SQLite sql-запросом и без объяснений.\n',
+            2: ''
+        }
+        return Variations[number]
+
+    def include_few_shot(self, number: int):
+        if self.__few_shot is None:
+            raise IndexError('Не добавлен few_shot')
+
+        Variations = {
+            1: f'### Примеры похожих запросов и ответы на них:\n{self.__few_shot}\n',
+            2: ''
+        }
+        return Variations[number]
+
+    def include_schema_template(self, number: int):
+        if self.__schema_template is None:
+            raise IndexError('Не добавлен schema_template')
+
+        Variations = {
+            1: f'### Схема таблиц:\n{self.__schema_template}\n',
+            2: ''
+        }
+        return Variations[number]
+
+    def include_cell_value_referencing(self, number: int):
+        if self.__cell_value_referencing is None:
+            raise IndexError('Не добавлен cell_value_referencing')
+
+        Variations = {
+            1: f'### Примеры данных в таблице:\n{self.__cell_value_referencing}\n',
+            2: ''
+        }
+        return Variations[number]
+
+    def include_question(self, number: int):
+        Variations = {
+            1: f'### Вопрос: {self.__question}\n### SQL:\n\n',
+            2: ''
+        }
+        return Variations[number]
+
+    def build_prompt(self, number: int):
+        Variations = {
+            1: {
+                'include_target': 1,
+                'include_few_shot': 1,
+                'include_schema_template': 1,
+                'include_cell_value_referencing': 1,
+                'include_question': 1
+            },
+            2: {
+                'include_question': 1
+            }
+        }
+
+        for func_name, value in Variations[number].items():
+            if hasattr(self, func_name):  # Проверяем, существует ли метод у объекта
+                func = getattr(self, func_name) 
+                self.__prompt += func(value)  
+            else:
+                print(f"Функция {func_name} не найдена")
+
         return self.__prompt
